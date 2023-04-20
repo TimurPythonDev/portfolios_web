08cddb562134f09cdba2bc9d411f638cf389be7e
TimurPythonDev <karabayevtimurpython@gmail.com>
2023-04-20 22:13:12 +0300

    models

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
+
+
+
+class ImageHomePage(models.Model):
+	image = models.ImageField(upload_to='static/user_img')
+
+
+class MySocialMedia(models.Model):
+	title		= models.CharFields(max_length=120)
+	icon		= models.CharFields(max_length=250)
+	icon_info 	= moels.CharFields(max_length=220)
+
+	def __str__(self):
+		return self.title
 
 
 
@@ -8,6 +23,7 @@ class AboutMe(models.Model):
 	""" About me models """
 
 	name 			= models.CharFields(max_length=120,verbose_name='Portfolio name')
+	image 			= models.ImageField(upload_to='static/user_img')
 	data_brith 		= models.DateField(u'Day of the data brith', help_text=u'Day of the event')
 	address 		= models.CharFields(max_length=200,verbose_name='Portfolio address')
 	email 			= models.CharFields(max_length=100,verbose_name='Portfolio email')
@@ -54,4 +70,100 @@ class LanguageSkills(models.Model):
 
 	def __str__(self):
 		return self.title
-					
+
+
+
+class ProjectForingKeyName(models.Model):
+    title =  models.CharField(max_length=255,verbose_name='Project title')
+
+    class Meta:
+    	verbose_name = 'Project Name'
+
+    def __str__(self):
+        return self.title
+
+
+
+class ProjectName(models.Model):
+
+    title 			= models.ForeignKey(ProjectForingKeyName,on_delete=models.CASCADE)
+    image 			= models.ImageField(upload_to='static/project_img')
+    year_completed  = models.DateTimeField(auto_now_add= True)
+    slug 			= models.SlugField(null=True, blank=True)
+    created 		= models.DateTimeField(auto_now_add=True)
+    description 	= RichTextUploadingField()
+    id 				= models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+
+    class Meta:
+        verbose_name = 'Project Name'
+
+    def yearpublished(self):
+        return self.year_completed.strftime('%Y')
+
+    def __str__(self):
+        return self.title
+
+
+class ProjectTag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+    	return self.name
+
+class ProjectComment(models.Model):
+    project = models.ForeignKey(ProjectName, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.body[0:50]
+
+
+class MyBlog(models.Model):
+    title = models.CharField(max_length=200)
+    thumbnail = models.ImageField(null=True)
+    body = RichTextUploadingField()
+    tags = models.ForeignKey(Tag,on_delete=models.CASCADE)
+    slug = models.SlugField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'My Blog'
+
+
+
+class BlogComment(models.Model):
+    project = models.ForeignKey(MyBlog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4,  unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
