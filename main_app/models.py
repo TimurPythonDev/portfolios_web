f472398c89470987b05575dec36163dfabeef240
TimurPythonDev <karabayevtimurpython@gmail.com>
2023-04-20 22:09:32 +0300

    created models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class AboutMe(models.Model):
+
+	""" About me models """
+
+	name 			= models.CharFields(max_length=120,verbose_name='Portfolio name')
+	data_brith 		= models.DateField(u'Day of the data brith', help_text=u'Day of the event')
+	address 		= models.CharFields(max_length=200,verbose_name='Portfolio address')
+	email 			= models.CharFields(max_length=100,verbose_name='Portfolio email')
+	phone_number 	= models.CharFields(max_length=20,verbose_name='Portfolio phone number')
+	complate_project= models.IntegerField()
+	document 		= models.FileField(upload_to='documents/',verbose_name='Upload You CV')
+
+	
+	class Meta:
+		verbose_name = 'About Me Data'
+
+	def __str__(self):
+		return self.name
+
+
+
+class Experiance(models.Model):
+
+	""" Experiance models """
+	year 			= models.DateField(u'Day of the Experiance', help_text=u'Day of the event')
+	posisions 		= models.CharFields(max_length=200,verbose_name='Experiance posisions') 
+	company_name	= models.CharFields(max_length=255,verbose_name'Company name')
+	descriptions  	= RichTextUploadingField()
+
+
+	class Meta:
+		verbose_name = 'My Experiance'
+
+	def __str__(self):
+		return self.posisions
+
+
+
+class LanguageSkills(models.Model):
+	""" Language Skills """
+	title 			= models.CharFields(max_length=100,verbose_name='Skills title')
+	skill_style_with= models.CharFields(max_length=10,verbose_name='Skills style with')
+	skill_style_span= models.CharFields(max_length=10,verbose_name='Skills style span')
+
+
+	class Meta:
+		verbose_name = 'Language Skills'
+
+
+	def __str__(self):
+		return self.title
+					
--- portfolios/settings.py
+++ portfolios/settings.py
@@ -41,6 +41,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Local app
+    'main_app',
+
+    # Librarias
+    'ckeditor',
+    'ckeditor_uploader',
+
 ]
 
 MIDDLEWARE = [
@@ -58,7 +65,7 @@ ROOT_URLCONF = 'portfolios.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join('BASE_DIR,','templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,8 +129,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = "/static/"
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
